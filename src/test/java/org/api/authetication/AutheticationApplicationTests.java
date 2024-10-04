29b1eb6f99f3cccbc5264394f858ea04043a2112
Gabriel <gabriel.antiqueira8@gmail.com>
2024-10-04 00:20:00 -0300

    feat(base-project)

---
+++ src/test/java/org/api/authetication/AutheticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.authetication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutheticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
