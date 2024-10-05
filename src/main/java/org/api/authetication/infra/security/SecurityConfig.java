fe263ba74ec8209923427330a6d4f377e305b1dc
Gabriel <gabriel.mantiqueira8@gmail.com>
2024-10-04 23:17:16 -0300

    feat:h2fix

--- src/main/java/org/api/authetication/infra/security/SecurityConfig.java
+++ src/main/java/org/api/authetication/infra/security/SecurityConfig.java
@@ -29,11 +29,13 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions().sameOrigin()) // Permitir que o H2 Console funcione
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.GET, "/auth/show").permitAll()
+                        .requestMatchers( "/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/org/api/authetication/service/UserService.java
+++ src/main/java/org/api/authetication/service/UserService.java
@@ -1,2 +1,24 @@
-package org.api.authetication.service;public class UserService {
+package org.api.authetication.service;
+
+
+import org.api.authetication.domain.user.User;
+import org.api.authetication.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return repository.save(user);
+    }
+
+    public User findUserByName(String name) {
+        return userRepository.findByName(name);
+    }
+
 }
